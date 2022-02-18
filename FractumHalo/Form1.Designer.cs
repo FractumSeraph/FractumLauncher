8ec60bd11c7753cae01109fe7a58035da84a92fc
User <User@FRACTUM-Z>
2022-02-17 18:26:36 -0600

    Add project files.

---
+++ FractumHalo/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace FractumHalo
+{
+    partial class Fractum
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fractum));
+            this.btnCheckFiles = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tbxInstallPath = new System.Windows.Forms.TextBox();
+            this.lblInstallPath = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.rtbMismatches = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheckFiles
+            // 
+            this.btnCheckFiles.Enabled = false;
+            this.btnCheckFiles.Location = new System.Drawing.Point(326, 183);
+            this.btnCheckFiles.Name = "btnCheckFiles";
+            this.btnCheckFiles.Size = new System.Drawing.Size(111, 23);
+            this.btnCheckFiles.TabIndex = 0;
+            this.btnCheckFiles.Text = "Check Files";
+            this.btnCheckFiles.UseVisualStyleBackColor = true;
+            this.btnCheckFiles.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog1.SelectedPath = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Halo The Master Chief Collection";
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // tbxInstallPath
+            // 
+            this.tbxInstallPath.Enabled = false;
+            this.tbxInstallPath.Location = new System.Drawing.Point(38, 94);
+            this.tbxInstallPath.Name = "tbxInstallPath";
+            this.tbxInstallPath.Size = new System.Drawing.Size(318, 22);
+            this.tbxInstallPath.TabIndex = 1;
+            this.tbxInstallPath.TextChanged += new System.EventHandler(this.tbxInstallPath_TextChanged);
+            // 
+            // lblInstallPath
+            // 
+            this.lblInstallPath.AutoSize = true;
+            this.lblInstallPath.Location = new System.Drawing.Point(35, 74);
+            this.lblInstallPath.Name = "lblInstallPath";
+            this.lblInstallPath.Size = new System.Drawing.Size(186, 17);
+            this.lblInstallPath.TabIndex = 2;
+            this.lblInstallPath.Text = "Path where Halo is installed.";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(362, 94);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // rtbMismatches
+            // 
+            this.rtbMismatches.Enabled = false;
+            this.rtbMismatches.Location = new System.Drawing.Point(38, 234);
+            this.rtbMismatches.Name = "rtbMismatches";
+            this.rtbMismatches.Size = new System.Drawing.Size(399, 204);
+            this.rtbMismatches.TabIndex = 4;
+            this.rtbMismatches.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Hashes that do not match.";
+            // 
+            // Fractum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbMismatches);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblInstallPath);
+            this.Controls.Add(this.tbxInstallPath);
+            this.Controls.Add(this.btnCheckFiles);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Fractum";
+            this.Text = "Fractum Halo Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheckFiles;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox tbxInstallPath;
+        private System.Windows.Forms.Label lblInstallPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.RichTextBox rtbMismatches;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FractumHalo/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Net;
+
+namespace FractumHalo
+{
+    public partial class Fractum : Form
+    {
+        public Fractum()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Check Files Button Clicked!");
+            string installPath = folderBrowserDialog1.SelectedPath.ToString();
+
+            Console.WriteLine("Calling LoadChecksum File");
+            LoadChecksumFile();
+
+            //Looping through checksums dictionary and comparing.
+            Console.WriteLine("Starting checksum loop");
+
+            // TODO This should be a function, I'll change that later.
+           foreach(var line in Checksums) //Loop through each loaded hash and check if they are correct.
+            {
+                string filepathToCheck = installPath + "\\" + line.Key;
+                CheckIfHashesMatch(filepathToCheck, line.Value, line.Key);
+
+            }
+           
+        }
+
+
+        public Boolean CheckIfHashesMatch(string filepath, string hash, string subpath)
+        {
+            /*
+             * Reads file from the filepath and checks them against the checksums.md5 list. 
+             * If they do not match, it prepares a download link for said file.
+             */
+
+            using (var stream = new BufferedStream(File.OpenRead(filepath), 1200000)) //Read files from the path, with a 1,200,000 byte buffer size.
+            {
+                
+                {
+                    MD5 md5 = new MD5CryptoServiceProvider();
+                    byte[] checksum = md5.ComputeHash(stream);
+                    string result = BitConverter.ToString(checksum).Replace("-", String.Empty); //Make the result match the normal md5 look. (Lowercase, alphanumeric.)
+
+                    Console.WriteLine("Checksum for file " + filepath + " is " + result.ToLower());
+
+                    if (result.ToLower().Equals(hash))
+                    {
+                        Console.WriteLine("Hash Matches!");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hashes do NOT match! Adding to list. Calling Download fuction");
+                        rtbMismatches.AppendText(filepath + "\n");
+                        //TODO Get the value for given key. Add weburl to the front to make download url.
+                        string webhost = @"http://halo.fractum.games/Halo The Master Chief Collection/";
+                        string downloadURL = webhost + subpath;
+                        Console.WriteLine("DownloadURl is currently " + downloadURL);
+                        return false;
+                    }
+
+                }
+            }
+
+
+
+        }
+
+
+
+        Dictionary<String, string> checksums = new Dictionary<string, string>();
+
+        public Dictionary<string, string> Checksums { get => checksums; set => checksums = value; }
+
+        public void LoadChecksumFile()
+
+            /*
+             * Opens a checksums.md5 file from the location selected by the folderBrowser.
+             * Loads the contents into a dictionary using the path as a key, and the hash as the value.
+             */
+
+        {
+            using (System.IO.StreamReader streamreader = new System.IO.StreamReader(folderBrowserDialog1.SelectedPath.ToString() + "\\" + "checksums.md5"))
+            {
+                Console.WriteLine("LoadChecksumFile has been called. Starting streamreader.");
+                while (!streamreader.EndOfStream) // Keep reading until we get to the end.
+                {
+                    string splitMe = streamreader.ReadLine();
+                    string[] checksumLine = splitMe.Split(new[] { " *" }, StringSplitOptions.None); //Split at the colons
+                    
+                    if (checksumLine.Length != 2)// If we get an incorrect amount, log it in console.
+                    {
+                        Console.WriteLine("Checksum.Length != 2");
+                        continue;
+                    }
+                    else if (checksumLine.Length == 2) //  If there are 2 results, add them to the dictionary
+                    {
+                        Console.WriteLine(checksumLine[1] + ":" + checksumLine[0]);
+                        //Read order is flipped. Dictionary is stored as the filepath first as the key, and the md5 is the value.
+                        //This is because it's possible to have two of the same checksums in your install. (One file in two places.) But it's impossible to have two files at the same path.
+                        Checksums.Add(checksumLine[1].Trim(), checksumLine[0].Trim());
+                    }
+                    
+                }
+                Console.WriteLine("LoadChecksumFile complete.");
+            }
+
+        }
+
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            tbxInstallPath.Text = folderBrowserDialog1.SelectedPath.ToString();
+
+            Console.WriteLine("Install path set to " + folderBrowserDialog1.SelectedPath.ToString());
+        }
+
+        private void DownloadFileFromURL(string url, string savepath)
+        {
+            WebClient mywebclient = new WebClient();
+            mywebclient.DownloadFile(url, savepath);
+        }
+
+        private void tbxInstallPath_TextChanged(object sender, EventArgs e)
+        {
+            btnCheckFiles.Enabled = true;
+        }
+    }
+}
